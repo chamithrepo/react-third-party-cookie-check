35fbc341d3cac99ac44a50c76eec7c74ae459077
Chamith Madusanka <chamithadl24@gmail.com>
2022-08-15 00:21:27 +0530

    React hook added

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import { useThirdPartyCookieCheck } from './useThirdPartyCookieCheck';
 function App() {
+  const status = useThirdPartyCookieCheck()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Third-Party Cookies enabled? {status ? 'Yes' : 'No'}</h2>
     </div>
   );
 }
